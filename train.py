00c6b69b486509691bf1df026aa0f48cf81c8fb0
SamSamhuns <samhunsadamant@gmail.com>
2024-11-05 11:21:53 +0400

    Merge train test

--- train.py
+++ train.py
@@ -1,19 +1,73 @@
 import os
 import torch
 from torch import optim
-from torch.utils.data import DataLoader
+from sklearn.metrics import roc_auc_score, accuracy_score
 import numpy as np
+
 
 from model import autoencoder, encoder
 from utils.utils import weights_init_normal
 
 
 class TrainerDeepSVDD:
-    def __init__(self, args, data, device):
+    def __init__(self, args, dls, device):
         self.args = args
-        self.train_loader, self.test_loader = data
+        self.train_loader, self.test_loader = dls
         self.device = device
         self.encoder = encoder().to(self.device)
+        self.c = None
+
+    def save_ae_weights(self, model, dataloader):
+        """Initialize Deep SVDD weights using the encoder weights of the pretrained autoencoder."""
+        os.makedirs("weights", exist_ok=True)
+        c = self.set_c(model, dataloader)
+        self.encoder = encoder(self.args.latent_dim).to(self.device)
+        state_dict = model.state_dict()
+        self.encoder.load_state_dict(state_dict, strict=False)
+        torch.save({"center": c.cpu().data.numpy().tolist(),
+                    "net_dict": self.encoder.state_dict()}, "weights/pretrained_parameters.pth")
+   
+    def save_enc_weights(self, model, epoch):
+        """Saving the Deep SVDD encoder model weights"""
+        os.makedirs("weights", exist_ok=True)
+        torch.save(model.state_dict(), f"weights/model_epoch_{epoch}.pth")
+
+    def set_c(self, model, dataloader, eps=0.1):
+        """Initializing the center for the hypersphere"""
+        model.eval()
+        z_ = []
+        with torch.no_grad():
+            for x, _ in dataloader:
+                x = x.float().to(self.device)
+                z = model.encode(x)
+                z_.append(z.detach())
+        z_ = torch.cat(z_)
+        c = torch.mean(z_, dim=0)
+        c[(abs(c) < eps) & (c < 0)] = -eps
+        c[(abs(c) < eps) & (c > 0)] = eps
+        return c
+
+    def calc_threshold_score(self, train_loader, model, percentile: float = 0.99) -> float:
+        """Calculate the anomaly threshold score for the model after training.
+
+        Args:
+            train_loader (DataLoader): DataLoader for the training dataset.
+            model (BaseModel): The Deep SVDD network.
+            percentile (float): Confidence percentile for threshold calculation.
+
+        Returns:
+            float: Anomaly threshold score.
+        """
+        model.eval()
+        dists = torch.tensor([], device=self.device)
+        with torch.no_grad():
+            for x, _ in train_loader:
+                x = x.to(self.device)
+                outputs = model(x)
+                dist = torch.sum((outputs - self.c) ** 2, dim=1)
+                dists = torch.cat((dists, dist), dim=0)
+
+        return np.percentile(dists.cpu().numpy(), percentile * 100)
 
     def pretrain(self):
         """ Pretraining the weights for the deep SVDD network using autoencoder"""
@@ -40,62 +94,9 @@ class TrainerDeepSVDD:
 
                 total_loss += reconst_loss.item()
             scheduler.step()
-            print(f"Epoch: {epoch}, Loss: {total_loss/len(self.train_loader):.3f}")
+            print(f"Epoch: {epoch}, Loss: {
+                  total_loss/len(self.train_loader):.3f}")
         self.save_ae_weights(ae, self.train_loader)
-
-    def save_ae_weights(self, model, dataloader):
-        """Initialize Deep SVDD weights using the encoder weights of the pretrained autoencoder."""
-        os.makedirs("weights", exist_ok=True)
-        c = self.set_c(model, dataloader)
-        self.encoder = encoder(self.args.latent_dim).to(self.device)
-        state_dict = model.state_dict()
-        self.encoder.load_state_dict(state_dict, strict=False)
-        torch.save({"center": c.cpu().data.numpy().tolist(),
-                    "net_dict": self.encoder.state_dict()}, "weights/pretrained_parameters.pth")
-        
-    def save_enc_weights(self, model, epoch):
-        """Saving the Deep SVDD encoder model weights"""
-        os.makedirs("weights", exist_ok=True)
-        torch.save(model.state_dict(), f"weights/model_epoch_{epoch}.pth")
-
-    def set_c(self, model, dataloader, eps=0.1):
-        """Initializing the center for the hypersphere"""
-        model.eval()
-        z_ = []
-        with torch.no_grad():
-            for x, _ in dataloader:
-                x = x.float().to(self.device)
-                z = model.encode(x)
-                z_.append(z.detach())
-        z_ = torch.cat(z_)
-        c = torch.mean(z_, dim=0)
-        c[(abs(c) < eps) & (c < 0)] = -eps
-        c[(abs(c) < eps) & (c > 0)] = eps
-        return c
-
-    def calc_threshold_score(
-            self, train_loader: DataLoader, model: torch.nn.Module, percentile: float = 0.99) -> float:
-        """Calculate the anomaly threshold score for the model after training.
-
-        Args:
-            train_loader (DataLoader): DataLoader for the training dataset.
-            model (BaseModel): The Deep SVDD network.
-            percentile (float): Confidence percentile for threshold calculation.
-
-        Returns:
-            float: Anomaly threshold score.
-        """
-        model.eval()
-        dists = torch.tensor([], device=self.device)
-        with torch.no_grad():
-            for data in train_loader:
-                inputs, _ = data
-                inputs = inputs.to(self.device)
-                outputs = model(inputs)
-                dist = torch.sum((outputs - self.c) ** 2, dim=1)
-                dists = torch.cat((dists, dist), dim=0)
-
-        return np.percentile(dists.cpu().numpy(), percentile * 100)
 
     def train(self):
         """Training the Deep SVDD model"""
@@ -131,4 +132,27 @@ class TrainerDeepSVDD:
             print(f"Epoch: {epoch}, Loss: {total_loss/len(self.train_loader):.3f}")
         self.c = c
         self.save_enc_weights(self.encoder, epoch)
-    
+
+    def eval_enc(self, threshold: float = 0.5):
+        """Evaluate the Deep SVDD encoder model"""
+
+        scores = []
+        labels = []
+        self.encoder.eval()
+        print("Testing Deep SVDD...")
+        print(f"Using a clsf threshold of {threshold}")
+        with torch.no_grad():
+            for x, y in self.test_loader:
+                x = x.float().to(self.device)
+                z = self.encoder(x)
+                score = torch.sum((z - self.c) ** 2, dim=1)
+
+                scores.append(score.detach().cpu())
+                labels.append(y.cpu())
+
+        labels, scores = torch.cat(labels).numpy(), torch.cat(scores).numpy()
+        print(f"ROC AUC score: {roc_auc_score(labels, scores)*100:.2f}")
+        print(f"Accuracy score: {accuracy_score(
+            labels, scores > threshold)*100:.2f}")
+
+        return labels, scores
